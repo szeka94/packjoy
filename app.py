642e93856bd6b8aa973b51f43ebbca0e8e7e22c1
Attila Szekely <attila.szekely@clever-software-solutions.com>
2017-08-04 19:39:17 +0300

    adding bootsrap in progress

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello at Pack-Joy'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
